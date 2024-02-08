d6d6bb29e166363840d2d5eb07755d4112e85c83
georgeaidonis <gaidonis90@gmail.com>
2024-02-08 15:56:02 +0200

    Add test case and implementation

--- test/UnitTests.BowlingScore/UnitTest1.cs
+++ test/UnitTests.BowlingScore/UnitTest1.cs
@@ -1,10 +1,25 @@
-namespace UnitTests.BowlingScore;
+namespace BowlingScore
 
-[TestClass]
-public class UnitTest1
 {
-    [TestMethod]
-    public void TestMethod1()
+    [TestClass]
+    public class UnitTest1
     {
+        [TestMethod]
+        [DataRow(new int[] { 0 }, 0)]
+        [DataRow(new int[] { 3 }, 3)]
+        [DataRow(new int[] { 3, 3 }, 6)]
+        [DataRow(new int[] { 3, 3, 3}, 9)]
+        [DataRow(new int[] { 3, 7, 3}, 16)]
+        [DataRow(new int[] { 10, 0, 3 }, 16)]
+        [DataRow(new int[] { 3, 7, 3, 3}, 19)]
+        // [DataRow(new int[] { 10, 0, 3, 3 }, 22)]
+        public void TestRunningScore(int[] rolls, int expectedCurrentScore)
+        {
+            // ACT
+            int actualCurrentScore = BowlingScore.getRunningScore(rolls);
+
+            // ASSERT
+            Assert.AreEqual(expectedCurrentScore, actualCurrentScore);
+        }
     }
 }
